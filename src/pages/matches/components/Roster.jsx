8baa8c5d01cdfcbed622810faa4459474aa31f30
alonso <glezalonso23@gmail.com>
2023-07-03 21:13:25 -0600

    filter roster

--- src/pages/matches/components/Roster.jsx
+++ src/pages/matches/components/Roster.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import { Button, Table, Alert } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Table, Alert, FormControl } from 'react-bootstrap'
 const Roaster = ({ match, roster, handleAddLineUp, type }) => {
+  const [dataFilter, setDataFilter] = useState('')
+
+  const players = roster?.map(team => team?.players?.map(player => player.playerId))
+
+  const filter = players?.map(players => players.filter(player => {
+    if (dataFilter) return player?.fullName?.toLowerCase().includes(dataFilter?.toLowerCase())
+    else return player
+  }))
+
   return (
         <>
-         {(roster?.length > 0)
+         <div className='mx-2 my-3'>
+            <h5 className="h5 static">All Teams</h5>
+            <FormControl placeholder='Search players...' id='filter' name='filter' value={dataFilter} onChange={(event) => setDataFilter(event.target.value)} />
+            </div>
+         {(filter?.length > 0)
            ? <div className='table-wrapper-scroll-y my-custom-scrollbar rounded'><Table responsive variant="dark table-sm" hover>
             <thead>
              <tr>
@@ -13,8 +26,8 @@ const Roaster = ({ match, roster, handleAddLineUp, type }) => {
              </tr>
         </thead>
         <tbody>
-                {roster?.map(team => team?.players?.map(player => (
-                    <tr key={player?.playerId?._id}><td>{player.playerId?.fullName}</td><td>{player?.playerId?.position}</td><td><Button variant="warning btn-sm" onClick={() => handleAddLineUp(match?._id, player?.playerId?._id, type)}>Add to lineup</Button></td></tr>
+                {filter?.map(players => players?.map(player => (
+                    <tr key={player?._id}><td>{player?.fullName}</td><td>{player?.position}</td><td><Button variant="warning btn-sm" onClick={() => handleAddLineUp(match?._id, player?._id, type)}>Add to lineup</Button></td></tr>
                 )))}
 
          </tbody>
